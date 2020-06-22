14b16a7fe8e9fad6341dd9a3221cbfcc86817ffc
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-06-22 22:11:27 +0600

    firebase initialized

---
+++ firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDK8OZ7Vc1QyQFS8JncfDsKmTluCfv8DeY",
+    authDomain: "ema-john-simple-7d026.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-7d026.firebaseio.com",
+    projectId: "ema-john-simple-7d026",
+    storageBucket: "ema-john-simple-7d026.appspot.com",
+    messagingSenderId: "271615832981",
+    appId: "1:271615832981:web:738597619bb848c8187c9c"
+};
+
+export default firebaseConfig;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from '../firebase.config';
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const provider = new firebase.auth.GoogleAuthProvider();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Sign In</button>
     </div>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
+  
