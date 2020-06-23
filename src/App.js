f8671083f1a00961184c75ba419f6914d504675c
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-06-23 22:43:19 +0600

    sign with google added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import * as firebase from "firebase/app";
 import "firebase/auth";
-import firebaseConfig from '../firebase.config';
+import firebaseConfig from './firebase.config';
 firebase.initializeApp(firebaseConfig);
 
 function App() {
   const provider = new firebase.auth.GoogleAuthProvider();
+  const [user, setUser] = useState( {
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photo: ''
+  } );
+  const handleSignIn = () => {
+    firebase.auth().signInWithPopup( provider )
+    .then( result => {
+      const {displayName, email, photoURL} = result.user;
+      const signedInUser = {
+        isSignedIn: true,
+        name: displayName,
+        email: email,
+        photo: photoURL
+      };
+      setUser(signedInUser);
+      console.log(displayName, email, photoURL);
+      
+    } )
+    .catch( error => {
+      console.log(error);
+      
+    } );
+    
+  }
   return (
     <div className="App">
-      <button>Sign In</button>
+      <button onClick={handleSignIn}>Sign In</button>
+      {
+        user.isSignedIn &&
+        <div>
+          <h2>Welcome, {user.name}</h2>
+          <img className="App-logo" src={user.photo} alt=""/>
+        </div>
+
+      }
     </div>
     );
   }
